875d7ab392770f8107ff63986621d894fabaf4da
danish manyotra <danishmanyotra@gmail.com>
2025-05-04 17:04:14 +0530

    Added that I want to drink Chai

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ function App() {
     <>
       <h1 className="text-3xl font-serif bg-pink-300 h-screen flex items-center justify-center text-purple-800">Hello, I am
         Danish from Jammu!!</h1>
+
+      <p>I want to drink Chai !!</p>
     </>
   )
 }
