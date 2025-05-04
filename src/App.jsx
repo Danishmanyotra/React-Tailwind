ede2c9d735cb3ff8fa0ed464b0d680487cf19ba2
danish manyotra <danishmanyotra@gmail.com>
2025-05-04 16:54:48 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-serif bg-pink-300 h-screen flex items-center justify-center text-purple-800">Hello, I am
+        Danish from Jammu!!</h1>
+    </>
+  )
+}
+
+export default App
